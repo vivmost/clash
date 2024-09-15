9b30c2efa0d67cdd5c4c55b61470a566ba373fc2
vivmost <vivmost@gmail.com>
2024-09-15 18:49:26 +0530

    routing set &  not found made

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,11 @@
+import HeroSection from '@/components/base/HeroSection'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
-      <h1 className='text-blue-600'> .... HOME PAGE ....</h1>
-      <Button> hello shadcn </Button>
+        <HeroSection />
     </div>
   )
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,13 @@
-import express, { Application, Request, Response} from "express";
+import express, { Application, Request, Response } from "express";
 import "dotenv/config";
 
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
 
 app.get("/", (req: Request, res: Response) => {
-    return res.send("Hey I'm UP and Working...");
-})
+  return res.send("Hey I'm UP and Working...");
+});
 
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port ${PORT}`);
-})
-
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
